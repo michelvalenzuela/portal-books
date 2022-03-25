ccda8fe1d27f36399b6c6d7761d82876f234cf48
michel <michel.valenzuela.bravo@gmail.com>
2022-03-25 14:07:42 -0300

    portal libros hecho..

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'books/avaliable'
+  get 'books/reserved'
+  get 'books/bought'
+
+  root 'books#avaliable'
+
+  get 'get_book/:book_id/:option', to: 'books#get_book', as: 'get_book_info'
+  post 'reserve/:book_id', to: 'books#reserve_book', as: 'reserve_book'
+  post 'unreserve/:book_id', to: 'books#unreserve_book', as: 'unreserve_book'
+  post 'pay', to: 'books#pay_book', as: 'pay_book'
+  delete 'delete/:book_id', to: 'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
